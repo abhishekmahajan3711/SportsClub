22bd9f8478ec6cc9b490d5678740ce075a33ddfa
abhishekmahajan3711 <118872502+abhishekmahajan3711@users.noreply.github.com>
2023-01-28 23:25:08 +0530

    sports club files are added

---
+++ java_file.js
@@ -0,0 +1,19 @@
+
+$("h1").css("color","#fff");
+
+function foot_member(){
+    alert("Football Members are- "+"\n"+"\t"+"1)ABCD"+"\n"+"\t"+"2)XYZE"+"\n"+"\t"+"3)MNOP"+"\n"+"\t"+"4)UVWD")
+}
+
+function bask_member(){
+    alert("Basketball Members are- "+"\n"+"\t"+"1)ABCD"+"\n"+"\t"+"2)XYZE"+"\n"+"\t"+"3)MNOP"+"\n"+"\t"+"4)UVWD")
+}
+
+function bad_member(){
+    alert("Badmiton Members are- "+"\n"+"\t"+"1)ABCD"+"\n"+"\t"+"2)XYZE"+"\n"+"\t"+"3)MNOP"+"\n"+"\t"+"4)UVWD")
+}
+
+function chess_member(){
+    alert("Chess Members are- "+"\n"+"\t"+"1)ABCD"+"\n"+"\t"+"2)XYZE"+"\n"+"\t"+"3)MNOP"+"\n"+"\t"+"4)UVWD")
+}
+
